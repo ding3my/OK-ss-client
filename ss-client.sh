694eb74bf7147e1c248b8956f9d6def235a23942
yjr <yjr@localhost.localdomain>
2017-03-08 23:56:09 +0800

    Wed Mar  8 23:56:09 CST 2017

--- ss-client.sh
+++ ss-client.sh
@@ -1,3 +1,4 @@
+sudo dnf install python-pip -y
 sudo pip install shadowsocks
 
 read -p"input remote host ip:" ip
@@ -16,7 +17,6 @@ end
 
 mkdir -p ~/.ss-client.d
 
-sudo dnf install python-pip -y
 sudo pip install genpac
 genpac --gfwlist-url=https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt -p"SOCKS5 127.0.0.1:1080" > ~/.ss-client.d/gfw.pac
 wget https://raw.githubusercontent.com/ding3my/OK-ss-client/master/all.pac -P ~/.ss-client.d
