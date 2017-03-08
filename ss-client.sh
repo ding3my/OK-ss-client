79ef853a15b7c83f5e7fd84dc8cb7d8cfb661ab6
yjr <yjr@localhost.localdomain>
2017-03-08 23:51:34 +0800

    Wed Mar  8 23:51:34 CST 2017

--- ss-client.sh
+++ ss-client.sh
@@ -16,7 +16,7 @@ end
 
 mkdir -p ~/.ss-client.d
 
-sudo dnf install pip
+sudo dnf install python-pip -y
 sudo pip install genpac
 genpac --gfwlist-url=https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt -p"SOCKS5 127.0.0.1:1080" > ~/.ss-client.d/gfw.pac
 wget https://raw.githubusercontent.com/ding3my/OK-ss-client/master/all.pac -P ~/.ss-client.d
